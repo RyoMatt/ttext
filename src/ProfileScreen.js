25d5793a9602d7cc0cbdbfe2291caeb04348188c
RyoMatt <52354219+RyoMatt@users.noreply.github.com>
2021-05-13 19:30:22 +0900

    Update ProfileScreen.js

--- src/ProfileScreen.js
+++ src/ProfileScreen.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
-
+import Post from './Post.js';
 
 const ProfileScreen = () => {
-  return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>ProfileScreen!</Text>
-      </View>
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>ProfileScreen!</Text>
+        </View>
     );
 }
 
