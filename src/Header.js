41a4c559753289c781f6c3162921917d44102b48
RyoMatt <52354219+RyoMatt@users.noreply.github.com>
2021-05-13 19:30:12 +0900

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,126 +1,115 @@
 import React,{useState} from 'react';
 import { Text, View, Image, StyleSheet,TouchableOpacity, Alert, TextInput} from 'react-native';
 
-const logoStyles = StyleSheet.create({
-  stretch: {
-    width: 200,
-    height: 50,
-    marginLeft: 20,
-    marginRight: 100,
-    marginBottom: 0,
-    marginTop: 0,
-    resizeMode: 'contain'
-  }
+const styles = StyleSheet.create({
+    logoStyles: {
+        width: 200,
+        height: 50,
+        marginLeft: 20,
+        marginRight: 100,
+        marginBottom: 0,
+        marginTop: 0,
+        resizeMode: 'contain'
+    },
+    searchStyles: {
+        width: 25,
+        height: 50,
+        marginLeft: 0,
+        marginBottom: 0,
+        marginTop: 0,
+        resizeMode: 'contain'
+    },
+    messageStyles: {
+        width: 35,
+        height: 50,
+        marginLeft: 10,
+        marginBottom: 0,
+        marginTop: 0,
+        resizeMode: 'contain'
+    },
+    inputStyles: {
+        width: 300,
+        height: 40,
+        marginLeft: 10,
+        marginBottom: 0,
+        marginTop: 5,
+        borderWidth: 1,
+    },
+    searchSetStyles: {
+        width: 25,
+        height: 50,
+        marginLeft: 15,
+        marginBottom: 0,
+        marginTop: 0,
+        resizeMode: 'contain'
+    },
+    cancelStyles: {
+        width: 20,
+        height: 50,
+        marginLeft: 10,
+        marginBottom: 0,
+        marginTop: 0,
+        resizeMode: 'contain'
+    }
 });
 
-const searchStyles = StyleSheet.create({
-  stretch: {
-    width: 25,
-    height: 50,
-    marginLeft: 0,
-    marginBottom: 0,
-    marginTop: 0,
-    resizeMode: 'contain'
-  }
-});
 
-const messageStyles = StyleSheet.create({
-  stretch: {
-    width: 35,
-    height: 50,
-    marginLeft: 10,
-    marginBottom: 0,
-    marginTop: 0,
-    resizeMode: 'contain'
-  }
-});
-
-const inputStyles = StyleSheet.create({
-  input: {
-    width: 300,
-    height: 40,
-    marginLeft: 10,
-    marginBottom: 0,
-    marginTop: 5,
-    borderWidth: 1,
-  },
-});
-
-const searchSetStyles = StyleSheet.create({
-  stretch: {
-    width: 25,
-    height: 50,
-    marginLeft: 15,
-    marginBottom: 0,
-    marginTop: 0,
-    resizeMode: 'contain'
-  }
-});
-
-const cancelStyles = StyleSheet.create({
-  stretch: {
-    width: 20,
-    height: 50,
-    marginLeft: 10,
-    marginBottom: 0,
-    marginTop: 0,
-    resizeMode: 'contain'
-  }
-});
 
 const Header = () => {
-  const [shouldShow, setShouldShow] = useState(true);
-  const [textInputValue, setTextInputValue] = React.useState('');
+    const [shouldShow, setShouldShow] = useState(true);
+    const [textInputValue, setTextInputValue] = React.useState('');
     return (
-    <View style={{flexDirection: "row",backgroundColor: '#F0000'}}>
-      {shouldShow ? (
-          <Image
-            style={logoStyles.stretch}
-            source={require('./logo_title.png')}
-          />
-      ) : null}
+        <View style={{flexDirection: "row",backgroundColor: '#F0000'}}>
+            {shouldShow ? (
+                <Image
+                  style={styles.logoStyles}
+                  source={require('./assets/logo_title.png')}
+                />
+            ) : null}
 
-      {!shouldShow ? (
-      <View style={{flexDirection: "row",backgroundColor: '#F0000'}}>
+            {!shouldShow ? (
+                <View style={{flexDirection: "row",backgroundColor: '#F0000'}}>
+                    <TouchableOpacity onPress={() => setShouldShow(!shouldShow)}>
+                        <Image
+                              style={styles.cancelStyles}
+                              source={require('./assets/cancel_icon.png')}
+                        />
+                    </TouchableOpacity>
+
+                    <TextInput
+                          style={styles.inputStyles}
+                          onChangeText={text => setTextInputValue(text)}
+                          value={textInputValue}
+                          placeholder="Search"
+                    />
+
+                    <TouchableOpacity onPress={() => Alert.alert('Searching')}>
+                        <Image
+                              style={styles.searchSetStyles}
+                              source={require('./assets/search_icon.png')}
+                        />
+                    </TouchableOpacity>
+                </View>
+            ) : null}
+
             <TouchableOpacity onPress={() => setShouldShow(!shouldShow)}>
-            <Image
-                  style={cancelStyles.stretch}
-                  source={require('./cancel_icon.png')}
-            />
+                {shouldShow ? (
+                    <Image
+                        style={styles.searchStyles}
+                        source={require('./assets/search_icon.png')}
+                    />
+                ) : null}
             </TouchableOpacity>
-            <TextInput
-                  style={inputStyles.input}
-                  onChangeText={text => setTextInputValue(text)}
-                  value={textInputValue}
-                  placeholder="Search"
-            />
-            <TouchableOpacity onPress={() => Alert.alert('Searching')}>
-            <Image
-                  style={searchSetStyles.stretch}
-                  source={require('./search_icon.png')}
-            />
-            </TouchableOpacity>
-      </View>
-      ) : null}
-
-      <TouchableOpacity onPress={() => setShouldShow(!shouldShow)}>
-      {shouldShow ? (
-            <Image
-                  style={searchStyles.stretch}
-                  source={require('./search_icon.png')}
-            />
-      ) : null}
-      </TouchableOpacity>
-      {shouldShow ? (
-      <TouchableOpacity onPress={() => Alert.alert('Message')}>
-            <Image
-                  style={messageStyles.stretch}
-                  source={require('./message_icon.png')}
-            />
-      </TouchableOpacity>
-      ) : null}
-    </View>
-  );
+            {shouldShow ? (
+                <TouchableOpacity onPress={() => Alert.alert('Message')}>
+                    <Image
+                        style={styles.messageStyles}
+                        source={require('./assets/message_icon.png')}
+                    />
+                </TouchableOpacity>
+            ) : null}
+        </View>
+    );
 };
 
 export default Header;
